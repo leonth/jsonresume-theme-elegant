b9b388cb3f2ff60edb871dc6e57466579094026e
Dave Umrysh <dave@umrysh.com>
2014-07-14 22:50:33 -0600

    Date formatting

--- index.js
+++ index.js
@@ -16,15 +16,36 @@ function render (resumeObject) {
 		});
 	}
 
-	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
-	var resumeHTML = Mustache.render(theme, resumeObject);
-	
 	_.each(resumeObject.work, function (work_experience) {
-		if (! work_experience.endDate) {
+		work_experience.startDate = work_experience.startDate.substr(0,4);
+		if (work_experience.endDate && work_experience.startDate){
+			if(work_experience.endDate.substr(0,4)==work_experience.startDate)
+			{
+				work_experience.endDate = "";
+			}else{
+				work_experience.endDate = work_experience.endDate.substr(0,4);
+			}
+		}else{
 			work_experience.endDate = 'Present';
 		}
 	});
-	
+
+	_.each(resumeObject.education, function(education_experience){
+		education_experience.startDate = education_experience.startDate.substr(0,4);
+		if (education_experience.endDate && education_experience.startDate){
+			if(education_experience.endDate.substr(0,4)==education_experience.startDate)
+			{
+				education_experience.endDate = "";
+			}else{
+				education_experience.endDate = education_experience.endDate.substr(0,4);
+			}
+		}else{
+			education_experience.endDate = 'Present';
+		}
+	});
+
+	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
+	var resumeHTML = Mustache.render(theme, resumeObject);
 	return resumeHTML;
 }
 
